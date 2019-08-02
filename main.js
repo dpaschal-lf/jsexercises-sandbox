d0665215553cedfa3b2d6657421155863e265aa8
Daniel <daniel.paschal@learningfuze.com>
2019-08-02 08:42:04 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+console.log( testCode( `
+/* prep code */
+var test = 60;
+var b = 2;
+/*end prep code */
+/* student code */
+
+
+test = test / 2;
+
+
+/* end student code */
+`))
+})
